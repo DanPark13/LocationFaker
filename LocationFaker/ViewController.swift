f0f4be64b173ea1fe0f186228c3ba545b5f0c339
DanPark13 <dpark0703@gmail.com>
2019-02-01 01:46:02 -0500

    CommitTest1

--- LocationFaker/ViewController.swift
+++ LocationFaker/ViewController.swift
@@ -6,15 +6,35 @@
 //  Copyright © 2018 shsmobiledeveloper. All rights reserved.
 //
 
+//all the imports
+import Foundation
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //calls the labels and progress view
+    @IBOutlet weak var status: UILabel!
+    @IBOutlet weak var progress: UIProgressView!
+    
+    //calls the function timor
+    var timer = Timer()
+    
+    
+    @IBAction func start(_ sender: Any) {
+        status.text = "Status: Loading..."
+        
+        timer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(ViewController.update), userInfo: nil, repeats: true)
+        
+        
+    }
+    
+    @objc func update(){
+        progress.progress += 0.05
+        if (progress.progress == 1.0){status.text = "Status: Complete"}
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
 }
-
